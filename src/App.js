4863de67abe705c20d6c8eae8579b4dc536b7061
James Hudson <jmh357@comcast.net>
2023-04-12 11:02:29 -0500

    finish adding lazy loading to blog and post pages

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { lazy, Suspense } from 'react'
 
 // import BlogPage, { loader as postsLoader } from './pages/Blog'
 import HomePage from './pages/Home'
-import PostPage, { loader as postLoader } from './pages/Post'
+// import PostPage, { loader as postLoader } from './pages/Post'
 import RootLayout from './pages/Root'
 
 // Lazy Loading
-
+const BlogPage = lazy(() => import('./pages/Blog'))
+const PostPage = lazy(() => import('./pages/Post'))
 
 const router = createBrowserRouter([
   {
@@ -20,8 +22,26 @@ const router = createBrowserRouter([
       {
         path: 'posts',
         children: [
-          { index: true, element: <BlogPage />, loader: () => import('./pages/Blog').then(module => module.loader()) },
-          { path: ':id', element: <PostPage />, loader: postLoader },
+          { 
+            index: true, 
+            element: (
+              <Suspense fallback={<p>Loading...</p>}>
+                <BlogPage />
+              </Suspense>
+            ), 
+            loader: () => 
+              import('./pages/Blog').then(module => module.loader()),
+          },
+          { 
+            path: ':id', 
+            element: (
+              <Suspense fallback={<p>Loading...</p>}>
+                <PostPage />
+              </Suspense>
+            ), 
+            loader: (meta) => 
+              import('./pages/Post').then(module => module.loader(meta)),
+          },
         ],
       },
     ],
